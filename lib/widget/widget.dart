d143bf8e8046d920e923ae822d0d6036519e6d84
Ashutosh Rai <ashutosh.developer78@gmail.com>
2022-08-05 13:13:12 +0530

    some changes in pubspec

--- lib/widget/widget.dart
+++ lib/widget/widget.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:wallpaper_app/model/wallpaper_model.dart';
 
 Widget brandName() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
-      Text("Wallpaper", style: TextStyle(color: Colors.black87),),
+      Text(
+        "Wallpaper",
+        style: TextStyle(color: Colors.black87),
+      ),
       Text("Hub", style: TextStyle(color: Colors.blue))
     ],
+  );
+}
+
+Widget WallpaperList(List<WallpaperModel> wallpapers, context) {
+  return Container(
+    child: GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: 0.6,
+    ),
   );
 }
